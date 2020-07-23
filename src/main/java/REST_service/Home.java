d94e3eebbb15e3a9a62b5e841b897fb6be184f6f
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-07-23 03:14:31 -0300

    feat: add new service to response a json

--- src/main/java/REST_service/Home.java
+++ src/main/java/REST_service/Home.java
@@ -4,17 +4,8 @@ import org.springframework.boot.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
 
-@RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Home {
-    @RequestMapping("/")
-    String home () {
-        return "Home page!";
-    }
-    @RequestMapping("/test")
-    String test () {
-        return "this is a test page";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Home.class, args);
